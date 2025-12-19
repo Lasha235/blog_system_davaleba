46b5243a2e18759dd0d06a0a20e35adc59d2c221
Lasha235 <gvatua.lasha1@gmail.com>
2025-12-19 19:12:51 +0400

    blog system

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.select_related('author', 'author__profile')
+
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(
+        Post.objects.prefetch_related('comments', 'comments__user', 'tags'), id=post_id)
+
+    return render(request, 'blog/post_detail.html', {'post': post})
